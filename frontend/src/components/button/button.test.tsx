321d697ecdee57f89f3aaae8930abcce9023733a
DAHUO-Melbourne <huodamelbourne@gmail.com>
2024-03-03 22:44:45 +1100

    test: add test to button component

---
+++ frontend/src/components/button/button.test.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {fireEvent, render, screen} from '@testing-library/react';
+import Button from "./Button";
+
+const defaultProps = {
+  onClick: jest.fn()
+};
+
+describe('test Button component', () => {
+  test('should render the correct default button', () => {
+    render(<Button {...defaultProps}>TEXT</Button>);
+    const element = screen.queryByText('TEXT');
+    expect(element).toBeTruthy();
+    expect(element?.tagName).toEqual('BUTTON');
+    expect(element?.classList.contains('button')).toBe(true);
+    element && fireEvent.click(element);
+    expect(defaultProps.onClick).toHaveBeenCalled();
+  });
+})
